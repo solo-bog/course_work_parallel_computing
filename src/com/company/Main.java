cb1f9af03107ce63e2111f929e0ef26c52cf70f9
solo-bog <bsoloxa@gmail.com>
2021-05-20 11:27:50 +0300

    add console usage

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,17 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        String inputs = "film,very,love";
+        try{
+            if(args.length < 2) throw new Exception("Enter data directory path and searched words");
+        }catch (Exception e){
+            e.printStackTrace();
+            return;
+        }
         Index idx = new Index();
-        idx.serialIndexing("./src/data/");
-        idx.search(Arrays.asList(inputs.split(",")));
+        idx.serialIndexing(args[0]);
+        idx.search(Arrays.asList(args[1].split(",")));
         idx.clear();
-        idx.parallelIndexing("./src/data/",2);
-        idx.search(Arrays.asList(inputs.split(",")));
+        idx.parallelIndexing(args[0],4);
+        idx.search(Arrays.asList(args[1].split(",")));
     }
 }
